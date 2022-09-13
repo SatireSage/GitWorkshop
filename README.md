b511906985cc28e54611112b26d19d0f42156565
Sahaj Singh <49692422+SatireSage@users.noreply.github.com>
2022-09-13 03:43:51 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Authors:
+[Sahaj Singh](https://github.com/SatireSage)
+
+# GitWorkshop
+The goal of this workshop is to teach participants how to use Git for version control and GitHub for collaborative coding. To gain a better understanding about the Git workflow, participants can fork this project and make changes.
+
+### Introduction:
+Version control systems allow multiple developers, and members to work together on the same project in an effective and efficient fashion. The system ensures that everyone has access to the latest code and that any modifications are tracked during the development process.
---
+++ list.js
@@ -0,0 +1,89 @@
+// Ignore this section 
+// x--------------------------------------------------------------------------------------------x
+class StarRating extends HTMLElement {
+    get value () {
+        return this.getAttribute('value') || 0;
+    }
+
+    set value (val) {
+        this.setAttribute('value', val);
+        this.highlight(this.value - 1);
+    }
+
+    get number () {
+        return this.getAttribute('number') || 5;
+    }
+
+    set number (val) {
+        this.setAttribute('number', val);
+
+        this.stars = [];
+
+        while (this.firstChild) {
+            this.removeChild(this.firstChild);
+        }
+
+        for (let i = 0; i < this.number; i++) {
+            let s = document.createElement('div');
+            s.className = 'star';
+            this.appendChild(s);
+            this.stars.push(s);
+        }
+
+        this.value = this.value;
+    }
+
+    highlight (index) {
+        this.stars.forEach((star, i) => {
+            star.classList.toggle('full', i <= index);
+        });
+    }
+
+    constructor () {
+        super();
+
+        this.number = this.number;
+
+        this.addEventListener('mousemove', e => {
+            let box = this.getBoundingClientRect(),
+                starIndex = Math.floor((e.pageX - box.left) / box.width * this.stars.length);
+
+            this.highlight(starIndex);
+        });
+
+        this.addEventListener('mouseout', () => {
+            this.value = this.value;
+        });
+
+        this.addEventListener('click', e => {
+            let box = this.getBoundingClientRect(),
+                starIndex = Math.floor((e.pageX - box.left) / box.width * this.stars.length);
+
+            this.value = starIndex + 1;
+
+            let rateEvent = new Event('rate');
+            this.dispatchEvent(rateEvent);
+        });
+    }
+}
+
+customElements.define('x-star-rating', StarRating);
+// x--------------------------------------------------------------------------------------------x
+
+// Edit the movie list below
+movie_list = [
+    "Harry Potter",
+    "Lord of the Rings",
+    "The Dark Knight",
+    "The Godfather",
+    "The Shawshank Redemption"
+  ];
+  
+// Do not edit below this line
+  movie_list.forEach((item) => {
+    listElement = document.createElement("div");
+    listElement.className = "item";
+    listElement.innerHTML += item;
+    listElement.innerHTML += '<x-star-rating value="2" number="5"></x-star-rating>';
+    document.getElementById("listContainer").appendChild(listElement);
+  });
